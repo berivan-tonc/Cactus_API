11c2e88b8da63614b0b2bc7ce4c6906a5cbd5135
FatmaKahraman <fatma.kahraman@ceng.deu.edu.tr>
2021-06-05 11:44:10 +0300

    deneme push

---
+++ cactus/Controllers/gitDeneme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cactus.Controllers
+{
+    public class GitDeneme
+    {
+        public static string GitDenemeFunc()
+        {
+            return "push deneme";
+
+        }
+    }
+}
