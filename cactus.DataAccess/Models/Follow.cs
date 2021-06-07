8b9e6789eb3ebcfd57c71d953c461e5e5fb6d1cc
FatmaKahraman <fatma.kahraman@ceng.deu.edu.tr>
2021-06-07 15:02:07 +0300

    Add follow and post models

---
+++ cactus.DataAccess/Models/Follow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace cactus.DataAccess.Models
+{
+    public class Follow
+    {
+        [Required, Key]
+        public int id { get; set; }
+
+        [Required]
+        public int following_id { get; set; }
+
+        [Required]
+        public int followied_id { get; set; }
+
+    }
+}
---
+++ cactus.DataAccess/Models/Post.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace cactus.DataAccess.Models
+{
+    public class Post
+    {
+        [Required, Key]
+        public int id { get; set; }
+
+        [Required]
+        public char category { get; set; }
+
+        [Required]
+        public int user_id { get; set; }
+
+        [Required]
+        public int book_id { get; set; }
+
+        [Required]
+        public int music_id { get; set; }
+
+        [Required]
+        public int movie_id { get; set; }
+
+        [Required]
+        public bool status { get; set; }
+
+        [Required]
+        public string comment { get; set; }
+
+        [Required]
+        public int point { get; set; }
+
+        [Required]
+        public DateTime editDate { get; set; }
+    }
+}
