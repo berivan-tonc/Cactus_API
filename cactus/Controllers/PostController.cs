ed9177c02e98eb0a544b5d23885472e4f85a809f
Berivan Tonç <Berivan.tonc@ceng.deu.edu.tr>
2021-06-15 00:31:59 +0300

    edit post set date now

--- cactus/Controllers/PostController.cs
+++ cactus/Controllers/PostController.cs
@@ -63,6 +63,7 @@ namespace cactus.Controllers
         [HttpPost]
         public async Task<Post> Post([FromBody] Post post)
         {
+            post.editdate = DateTime.Now;
             return await _postService.CreatePost(post);
         }
 
